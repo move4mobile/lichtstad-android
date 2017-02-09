1d77261edd204130dc449b7c9d98e3e69da7c66b
Wilco Wolters <wwolters@move4mobile.com>
2017-02-09 21:12:34 +0100

    Add shared element transition between fragments

--- app/src/main/java/com/move4mobile/lichtstad/MainActivity.java
+++ app/src/main/java/com/move4mobile/lichtstad/MainActivity.java
@@ -2,15 +2,18 @@ package com.move4mobile.lichtstad;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.transition.TransitionInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.move4mobile.lichtstad.databinding.ActivityMainBinding;
-import com.move4mobile.lichtstad.program.ProgramFragment;
 import com.move4mobile.lichtstad.photo.AlbumsFragment;
+import com.move4mobile.lichtstad.program.ProgramFragment;
 import com.move4mobile.lichtstad.result.ResultsFragment;
 import com.move4mobile.lichtstad.video.VideosFragment;
 
@@ -50,8 +53,15 @@ public class MainActivity extends Activity implements BottomNavigationView.OnNav
     }
 
     private void showFragment(Fragment fragment) {
-        getFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, fragment)
+        fragment.setSharedElementEnterTransition(TransitionInflater.from(this).inflateTransition(R.transition.shared_element));
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        View appBar = findViewById(R.id.appbar);
+        if (appBar != null) {
+            transaction.addSharedElement(appBar, appBar.getTransitionName());
+        }
+        transaction.replace(R.id.fragment_container, fragment)
                 .commit();
     }
 }
