10c5a574adbc82f5d754e115e4dbdec210001f81
Wilco Wolters <wwolters@move4mobile.com>
2017-02-13 21:30:43 +0100

    Fix ugly abbreviated names

--- app/src/main/java/com/move4mobile/lichtstad/program/ProgramPagerAdapter.java
+++ app/src/main/java/com/move4mobile/lichtstad/program/ProgramPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.move4mobile.lichtstad.program;
 
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
@@ -9,6 +10,7 @@ import android.support.v13.app.FragmentStatePagerAdapter;
 import com.move4mobile.lichtstad.R;
 
 import java.text.DateFormat;
+import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collections;
@@ -17,15 +19,16 @@ import java.util.List;
 public class ProgramPagerAdapter extends FragmentStatePagerAdapter {
 
     private List<Calendar> days;
-    private Context context;
     private DateFormat format;
 
+    @SuppressLint("SimpleDateFormat")
     public ProgramPagerAdapter(Context context, FragmentManager fm, List<Calendar> days) {
         super(fm);
-        this.context = context;
         this.days = Collections.unmodifiableList(days);
 
-        format = new SimpleDateFormat(context.getString(R.string.format_tab_date), context.getResources().getConfiguration().locale);
+        DateFormatSymbols symbols = DateFormatSymbols.getInstance(context.getResources().getConfiguration().locale);
+        symbols.setShortMonths(context.getResources().getStringArray(R.array.abbreviated_months));
+        format = new SimpleDateFormat(context.getString(R.string.format_tab_date), symbols);
     }
 
     @Override
