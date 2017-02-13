2806f72cd763664b836a4e4c1144eb1865d46faa
Wilco Wolters <wwolters@move4mobile.com>
2017-02-13 22:43:56 +0100

    Apply custom style to tab text

--- app/src/main/java/com/move4mobile/lichtstad/program/ProgramPagerAdapter.java
+++ app/src/main/java/com/move4mobile/lichtstad/program/ProgramPagerAdapter.java
@@ -6,6 +6,9 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.support.v13.app.FragmentStatePagerAdapter;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.RelativeSizeSpan;
 
 import com.move4mobile.lichtstad.R;
 
@@ -18,12 +21,14 @@ import java.util.List;
 
 public class ProgramPagerAdapter extends FragmentStatePagerAdapter {
 
+    private Context context;
     private List<Calendar> days;
     private DateFormat format;
 
     @SuppressLint("SimpleDateFormat")
     public ProgramPagerAdapter(Context context, FragmentManager fm, List<Calendar> days) {
         super(fm);
+        this.context = context;
         this.days = Collections.unmodifiableList(days);
 
         DateFormatSymbols symbols = DateFormatSymbols.getInstance(context.getResources().getConfiguration().locale);
@@ -43,6 +48,18 @@ public class ProgramPagerAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return format.format(days.get(position).getTime());
+        String formattedDate = format.format(days.get(position).getTime()).toUpperCase();
+        String[] lines = formattedDate.split("\n", 2);
+        if (lines.length == 0) {
+            throw new IllegalStateException();
+        }
+        if (lines.length == 1) {
+            return formattedDate;
+        }
+
+        return new SpannableStringBuilder()
+                .append(lines[0])
+                .append('\n')
+                .append(lines[1], new RelativeSizeSpan(1.25f), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
     }
 }
