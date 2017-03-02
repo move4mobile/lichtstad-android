59854e30c5b8b52a01d2e060d0f31ae019b8df26
Wilco Wolters <wwolters@users.noreply.github.com>
2017-03-02 14:36:41 +0100

    Feature/automatic release (#6)
    Automatically deploy to Play Store

---
+++ travis-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $TRAVIS_PULL_REQUEST != false ]
+then
+    echo "Pull request, not deploying"
+    bash ./gradlew assembleRelease
+elif [ "$TRAVIS_BRANCH" = 'master' ]
+then
+    echo "Push to master, deploying to beta"
+    EXPORT PLAY_TRACK='beta'
+    bash ./gradlew publishApkRelease
+elif [ "$TRAVIS_BRANCH" = 'develop' ]
+then
+    echo "Push to develop, deploying to alpha"
+    EXPORT PLAY_TRACK='alpha'
+    bash ./gradlew publishApkRelease
+else
+    echo "Push to $TRAVIS_BRANCH, not deploying"
+    bash ./gradlew assembleRelease
+fi
