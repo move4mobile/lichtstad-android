d64a12df4e64543bedde6e8ae92388d33d9bc347
Wilco Wolters <wwolters@move4mobile.com>
2018-06-27 20:21:33 +0200

    Configure maps sdk

--- app/src/main/java/com/move4mobile/lichtstad/map/MapFragment.java
+++ app/src/main/java/com/move4mobile/lichtstad/map/MapFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.SupportMapFragment;
 import com.move4mobile.lichtstad.BaseContentFragment;
 import com.move4mobile.lichtstad.R;
 import com.move4mobile.lichtstad.databinding.FragmentMapBinding;
@@ -18,6 +19,12 @@ public class MapFragment extends BaseContentFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         FragmentMapBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_map, container, false);
+
+        if (savedInstanceState == null) {
+            getChildFragmentManager().beginTransaction()
+                    .add(R.id.container, new SupportMapFragment())
+                    .commit();
+        }
 
         return binding.getRoot();
     }
